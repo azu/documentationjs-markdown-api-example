a3024e995003081ca2ebd9d74aa0aade48c4de7a
azu <azuciao@gmail.com>
2019-01-03 10:30:08 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * Return Hello message
+ * @param {string} name
+ * @returns {string}
+ */
+function hello(name) {
+    return `Hello, ${name}`;
+}
+
+/**
+ * update property with `propertyValue` if the `propertyName` does not exists
+ * @param {*} object
+ * @param {string} propertyName
+ * @param {string} propertyValue
+ * @param {{force: boolean}} options
+ */
+const updateProp = (object, propertyName, propertyValue, options = {}) => {
+    if (object[propertyName] && options.force) {
+        object[propertyName] = propertyValue;
+    } else if (object[propertyName]) {
+        // no update
+    } else {
+        object[propertyName] = propertyValue;
+    }
+};
+
+class Myclass {
+    /**
+     * report message
+     * @param {string} message
+     */
+    report(message) {
+
+    }
+}
+
+export {
+    hello,
+    updateProp
+}
